9eb42ac15b9540e05597c7e4de4ec85343932e5e
Alexander Rubinstein <rubalex14@gmail.com>
2025-06-14 21:17:05 +0200

    Add test for Condor config

--- tests/run_from_csv/test_main.py
+++ tests/run_from_csv/test_main.py
@@ -31,20 +31,41 @@ SMALL_SLEEP_TIME = 1
 SLEEP_TIME = 5
 
 
-def create_test_csv(csv_path, config_path):
+def create_test_csv(csv_path, config_path, cluster_type="slurm"):
     """Create a test CSV file with minimal required columns."""
     path_to_main = os.path.join(CUR_FOLDER, "executable.py")
-    data = {
-        "path_to_default_config": [config_path] * TOTAL_ROWS,
-        "path_to_main": [path_to_main] * TOTAL_ROWS,
-        "whether_to_run": ["1", "0", "1"],
-        "slurm:cpus-per-task": ["4"] * TOTAL_ROWS,
-        "slurm:partition": ["a100"] * TOTAL_ROWS,
-        "delta:logging/wandb/netrc_path": ["~/.netrc"] * TOTAL_ROWS,
-        "delta:logging/use_wandb": ["FALSE"] * TOTAL_ROWS,
-        "delta:initialization_type": ["zeros", "random", "ones"],
-        "delta:image/color": ["red"] * TOTAL_ROWS,
-    }
+    if cluster_type == "slurm":
+        data = {
+            "path_to_default_config": [config_path] * TOTAL_ROWS,
+            "path_to_main": [path_to_main] * TOTAL_ROWS,
+            "whether_to_run": ["1", "0", "1"],
+            "slurm:cpus-per-task": ["4"] * TOTAL_ROWS,
+            "slurm:partition": ["a100"] * TOTAL_ROWS,
+            "delta:logging/wandb/netrc_path": ["~/.netrc"] * TOTAL_ROWS,
+            "delta:logging/use_wandb": ["FALSE"] * TOTAL_ROWS,
+            "delta:initialization_type": ["zeros", "random", "ones"],
+            "delta:image/color": ["red"] * TOTAL_ROWS,
+        }
+    else:
+        assert cluster_type == "condor", "Invalid cluster type"
+        data = {
+            "path_to_default_config": [config_path],
+            "path_to_main": [path_to_main],
+            "whether_to_run": ["1"],
+            "condor:cpus-per-task": ["4"],
+            "condor:partition": ["a100"],
+            "condor:bid": ["1"],
+            "condor:output": ["logs/job_stdout.out"],
+            "condor:error": ["logs/job_stderr.err"],
+            "delta:logging/wandb/netrc_path": ["~/.netrc"],
+            "delta:logging/use_wandb": ["FALSE"],
+            "delta:initialization_type": ["zeros"],
+            "delta:image/color": ["red"],
+            "env_var:CUDA_VISIBLE_DEVICES": [
+                "0__COMMA__1__COMMA__2__COMMA__3__COMMA__4__COMMA__5__COMMA__6__COMMA__7"
+            ],
+        }
+
     df = pd.DataFrame(data)
     df.to_csv(csv_path, index=False)
 
@@ -82,10 +103,12 @@ def test_env(request):
         os.environ["PROJECT_ROOT_PROVIDED_FOR_STUNED"] = temp_dir
         # Create test files
         csv_path = os.path.join(temp_dir, "test.csv")
+        csv_path_condor = os.path.join(temp_dir, "test_condor.csv")
         config_path = os.path.join(CUR_FOLDER, "config.yaml")
 
         # optionally can create config inplace: create_test_config(config_path)
         create_test_csv(csv_path, config_path)
+        create_test_csv(csv_path_condor, config_path, cluster_type="condor")
 
         # Create necessary directories
         os.makedirs(os.path.join(temp_dir, "experiment_configs"), exist_ok=True)
@@ -94,6 +117,7 @@ def test_env(request):
         env_dict = {
             "temp_dir": temp_dir,
             "csv_path": csv_path,
+            "csv_path_condor": csv_path_condor,
             "config_path": config_path,
         }
 
@@ -217,42 +241,114 @@ def test_sbatch_scripts(test_env):
         "--debug",
     ]
 
+    check_submission_script(cmd, "slurm")
+
+
+# @pytest.mark.skip(reason="Temporarily disabled")
+def test_condor_scripts(test_env):
+    """Test that the condor scripts are created correctly."""
+
+    # Run the script with test environment
+    log_file_path = os.path.join(test_env["temp_dir"], "test_log_condor.out")
+
+    python_binary, conda_env = get_python_binary()
+    cmd = [
+        python_binary,
+        "-m",
+        "stnd.run_from_csv.__main__",
+        "--csv_path",
+        test_env["csv_path_condor"],
+        "--conda_env",
+        "<env_for_condor>",
+        "--log_file_path",
+        log_file_path,
+        "--debug",
+        "--cluster_type",
+        "condor",
+    ]
+    check_submission_script(cmd, "condor")
+
+
+def check_submission_script(cmd, cluster_type):
     # Run the command and check for errors
     result = subprocess.run(cmd, capture_output=True, text=True)
-    canonical_path = os.path.join(CUR_FOLDER, "canonical_sbatch.txt")
+
+    if cluster_type == "condor":
+        canonical_path = os.path.join(CUR_FOLDER, f"canonical_condor.txt")
+    else:
+        canonical_path = os.path.join(CUR_FOLDER, f"canonical_sbatch.txt")
     with open(canonical_path, "r") as f:
         canonical_contents = f.read()
+
+    if cluster_type == "condor":
+        with open(
+            os.path.join(CUR_FOLDER, f"canonical_condor_sh_file.txt"), "r"
+        ) as f:
+            canonical_contents_sh_file = f.read()
+    else:
+        canonical_contents_sh_file = None
+
+    condor_executable_path = None
     for line in result.stdout.split("\n"):
         if "COMMAND TO SUBMIT" in line:
-            sbatch_file = line.split("sbatch ")[-1]
+            if cluster_type == "condor":
+                submission_file = line.split("condor_submit_bid ")[-1].split(
+                    " "
+                )[-1]
+            else:
+                submission_file = line.split("sbatch ")[-1]
             # Read and compare sbatch file contents
-            with open(sbatch_file, "r") as f:
-                sbatch_contents = f.read()
+            with open(submission_file, "r") as f:
+                submission_contents = f.read()
 
-            # Replace random temp folder hash with generic <hash> placeholder
-            tmp_hash = sbatch_contents.split("/run_from_csv", 1)[-1].split("/")[
-                0
-            ]
-            sbatch_contents = sbatch_contents.replace(tmp_hash, "<hash>")
-            autogen_config = sbatch_contents.split("autogenerated/")[-1].split(
-                ".yaml"
-            )[0]
-            sbatch_contents = (
-                sbatch_contents.replace(
+            if cluster_type == "condor":
+                condor_executable_path = submission_contents.split(
+                    "executable = "
+                )[-1].split("\n")[0]
+                submission_contents = submission_contents.replace(
+                    condor_executable_path, "<executable>"
+                )
+                assert (
+                    submission_contents == canonical_contents
+                ), "Generated condor submission file does not match canonical file"
+                # Condor has 2 submission files, now reading the second one
+                with open(condor_executable_path, "r") as f:
+                    submission_contents = f.read()
+                assert canonical_contents_sh_file is not None
+                canonical_contents = canonical_contents_sh_file
+
+            else:
+                # Replace random temp folder hash with generic <hash> placeholder
+                tmp_hash = submission_contents.split("/run_from_csv", 1)[
+                    -1
+                ].split("/")[0]
+                submission_contents = submission_contents.replace(
+                    tmp_hash, "<hash>"
+                )
+                before_python = None
+                for line in submission_contents.split("\n"):
+                    if "python " in line:
+                        before_python = line.split("python ")[0]
+                assert before_python is not None, "python line not found"
+                submission_contents = submission_contents.replace(
+                    before_python, ""
+                )
+            autogen_config = submission_contents.split("autogenerated/")[
+                -1
+            ].split(".yaml")[0]
+            submission_contents = (
+                submission_contents.replace(
                     autogen_config, "<autogenerated_config>"
                 ).replace(STND_ROOT, ".")
                 + "\n"
             )
-            before_python = None
-            for line in sbatch_contents.split("\n"):
-                if "python " in line:
-                    before_python = line.split("python ")[0]
-            assert before_python is not None, "python line not found"
-            sbatch_contents = sbatch_contents.replace(before_python, "")
+            submission_contents = submission_contents.replace(
+                os.path.expanduser("~"), "~"
+            )
 
             assert (
-                sbatch_contents == canonical_contents
-            ), "Generated sbatch file does not match canonical file"
+                submission_contents == canonical_contents
+            ), "Submission file does not match canonical file"
 
 
 def cleanup_test_env(test_env):
