05ce393342a5fc7ec47239a87ad9a27c94617918
Alexander Rubinstein <rubalex14@gmail.com>
2025-06-15 12:19:10 +0200

    - Add test for runner - Restructure test folders

--- tests/run_from_csv/test_main.py
+++ tests/run_from_csv/test_main.py
@@ -7,6 +7,7 @@ import pandas as pd
 import sys
 import subprocess
 import time
+import numpy as np
 
 
 # local imports
@@ -20,6 +21,7 @@ from stnd.utility.utils import (
     optionally_make_parent_dir,
     run_cmd_through_popen,
 )
+from stnd.run_from_csv.__main__ import RUNNER_PLACEHOLDER
 
 sys.path.pop(0)
 
@@ -29,22 +31,42 @@ TOTAL_ROWS = 3
 MAX_SLEEPS = 100
 SMALL_SLEEP_TIME = 1
 SLEEP_TIME = 5
+# SKIP_TESTS = True
+SKIP_TESTS = False
 
 
-def create_test_csv(csv_path, config_path):
+CANONICAL_FOLDER = os.path.join(CUR_FOLDER, "canonical_files")
+EXECUTABLES_FOLDER = os.path.join(CUR_FOLDER, "executables")
+CONFIGS_FOLDER = os.path.join(CUR_FOLDER, "configs")
+
+
+def create_test_csv(csv_path, config_path, cluster_type="slurm"):
     """Create a test CSV file with minimal required columns."""
-    path_to_main = os.path.join(CUR_FOLDER, "executable.py")
-    data = {
-        "path_to_default_config": [config_path] * TOTAL_ROWS,
-        "path_to_main": [path_to_main] * TOTAL_ROWS,
-        "whether_to_run": ["1", "0", "1"],
-        "slurm:cpus-per-task": ["4"] * TOTAL_ROWS,
-        "slurm:partition": ["a100"] * TOTAL_ROWS,
-        "delta:logging/wandb/netrc_path": ["~/.netrc"] * TOTAL_ROWS,
-        "delta:logging/use_wandb": ["FALSE"] * TOTAL_ROWS,
-        "delta:initialization_type": ["zeros", "random", "ones"],
-        "delta:image/color": ["red"] * TOTAL_ROWS,
-    }
+    path_to_main = os.path.join(EXECUTABLES_FOLDER, "executable.py")
+    path_to_runner_target = os.path.join(EXECUTABLES_FOLDER, "runner_target.py")
+    if cluster_type == "slurm":
+        data = {
+            "path_to_default_config": [config_path] * TOTAL_ROWS,
+            "path_to_main": [path_to_main] * TOTAL_ROWS,
+            "whether_to_run": ["1", "0", "1"],
+            "slurm:cpus-per-task": ["4"] * TOTAL_ROWS,
+            "slurm:partition": ["a100"] * TOTAL_ROWS,
+            "delta:logging/wandb/netrc_path": ["~/.netrc"] * TOTAL_ROWS,
+            "delta:logging/use_wandb": ["FALSE"] * TOTAL_ROWS,
+            "delta:initialization_type": ["zeros", "random", "ones"],
+            "delta:image/color": ["red"] * TOTAL_ROWS,
+        }
+    elif cluster_type == "runner":
+        data = {
+            "path_to_default_config": [config_path],
+            "path_to_main": [RUNNER_PLACEHOLDER],
+            "whether_to_run": ["1"],
+            "delta:exec_path": [path_to_runner_target],
+            "delta:conda_env": ["None"],
+            "delta:is_python": ["True"],
+        }
+    else:
+        raise ValueError(f"Invalid cluster type: {cluster_type}")
     df = pd.DataFrame(data)
     df.to_csv(csv_path, index=False)
 
@@ -74,6 +96,7 @@ def test_env(request):
         print(f"temp_dir for testing: {temp_dir}")
 
         # Create symlink to parent repo's .git directory so git commands work
+        # git diff in logs will show that all the files are deleted
         parent_git_dir = os.path.join(
             os.path.dirname(os.path.dirname(CUR_FOLDER)), ".git"
         )
@@ -82,10 +105,15 @@ def test_env(request):
         os.environ["PROJECT_ROOT_PROVIDED_FOR_STUNED"] = temp_dir
         # Create test files
         csv_path = os.path.join(temp_dir, "test.csv")
-        config_path = os.path.join(CUR_FOLDER, "config.yaml")
+        csv_path_runner = os.path.join(temp_dir, "test_runner.csv")
+        config_path = os.path.join(CONFIGS_FOLDER, "config.yaml")
+        config_path_runner = os.path.join(CONFIGS_FOLDER, "runner_config.yaml")
 
         # optionally can create config inplace: create_test_config(config_path)
         create_test_csv(csv_path, config_path)
+        create_test_csv(
+            csv_path_runner, config_path_runner, cluster_type="runner"
+        )
 
         # Create necessary directories
         os.makedirs(os.path.join(temp_dir, "experiment_configs"), exist_ok=True)
@@ -94,6 +122,7 @@ def test_env(request):
         env_dict = {
             "temp_dir": temp_dir,
             "csv_path": csv_path,
+            "csv_path_runner": csv_path_runner,
             "config_path": config_path,
         }
 
@@ -119,7 +148,116 @@ def get_python_binary():
     return python_binary, conda_env
 
 
-# @pytest.mark.skip(reason="Temporarily disabled")
+def read_last_bytes(file_path, n_bytes=1024):
+    with open(file_path, "r") as f:
+        f.seek(0, 2)  # Seek to end of file
+        size = f.tell()  # Get file size
+        f.seek(max(0, size - n_bytes))  # Go back 1024 bytes from end
+        return f.read()
+
+
+@pytest.mark.skipif(SKIP_TESTS, reason="Skip tests when debugging")
+def test_runner_script(test_env):
+    """Test that the runner script runs without errors."""
+
+    # Run the script with test environment
+    log_file_path = os.path.join(test_env["temp_dir"], "test_log.out")
+
+    python_binary, conda_env = get_python_binary()
+    cmd = [
+        python_binary,
+        "-m",
+        "stnd.run_from_csv.__main__",
+        "--csv_path",
+        test_env["csv_path_runner"],
+        "--conda_env",
+        conda_env,
+        "--run_locally",
+        "--log_file_path",
+        log_file_path,
+    ]
+    df = compare_csv_to_canonical(
+        cmd,
+        log_file_path,
+        test_env["csv_path_runner"],
+        os.path.join(CANONICAL_FOLDER, "canonical_csv_runner.csv"),
+    )
+    run_folder = df["run_folder"].iloc[0]
+    stdout_path = os.path.join(test_env["temp_dir"], run_folder, "stdout.txt")
+    stderr_path = os.path.join(test_env["temp_dir"], run_folder, "stderr.txt")
+    stdout = read_last_bytes(stdout_path)
+    stderr = read_last_bytes(stderr_path)
+    assert "Hello, stdout" in stdout, "Hello, stdout not found"
+    assert "Hello, stderr" in stderr, "Hello, stderr not found"
+
+
+def compare_csv_to_canonical(cmd, log_file_path, df_path, canonical_df_path):
+    # Run the command and check for errors
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    time.sleep(SMALL_SLEEP_TIME)  # make sure that status column is created
+
+    # Check that the script ran successfully
+    assert result.returncode == 0, f"Script failed with error: {result.stderr}"
+
+    # Check that the output file was created
+    assert os.path.exists(log_file_path), "Log file was not created"
+
+    # Check that the CSV was updated
+    df = pd.read_csv(df_path)
+    num_sleeps = 0
+    while (
+        df["status"].iloc[0] in ["Submitted", "Running"]
+        and num_sleeps < MAX_SLEEPS
+    ):
+        # Wait a bit for the job to complete
+        time.sleep(SLEEP_TIME)
+        df = pd.read_csv(df_path)
+        num_sleeps += 1
+    assert df["status"].iloc[0] == "Completed", "CSV status was not updated"
+
+    # Load canonical CSV for comparison
+    canonical_df = pd.read_csv(canonical_df_path)
+
+    # Check that all expected columns exist in both dataframes
+    assert set(canonical_df.columns) == set(
+        df.columns
+    ), "CSV columns do not match canonical CSV"
+
+    # Check run_folder column exists and contains string
+    for col in UNIQUE_COLUMNS:
+        assert col in df.columns, f"{col} column missing"
+        assert isinstance(df[col].iloc[0], str), f"{col} value is not a string"
+
+    # Compare all columns except run_folder
+    cols_to_compare = [
+        col for col in canonical_df.columns if col not in UNIQUE_COLUMNS
+    ]
+    for col in cols_to_compare:
+        for row in range(canonical_df.shape[0]):
+            value_in_df = df[col].iloc[row]
+            if isinstance(value_in_df, str):
+                value_in_df = value_in_df.replace(CUR_FOLDER, ".")
+            canonical_value = canonical_df[col].iloc[row]
+            if not isinstance(canonical_value, str) and np.isnan(
+                canonical_value
+            ):
+                assert np.isnan(
+                    value_in_df
+                ), f"Value in column {col} should be NaN"
+            else:
+                assert (
+                    value_in_df == canonical_value
+                ), f"Column {col} does not match canonical CSV"
+
+            # Clean up autogenerated folder
+            autogen_path = os.path.join(CONFIGS_FOLDER, "autogenerated")
+            if os.path.exists(autogen_path):
+                shutil.rmtree(autogen_path)
+
+    return df
+
+
+@pytest.mark.skipif(SKIP_TESTS, reason="Skip tests when debugging")
 def test_main_script(test_env):
     """Test that the main script runs without errors."""
 
@@ -140,63 +278,15 @@ def test_main_script(test_env):
         log_file_path,
     ]
 
-    # Run the command and check for errors
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    time.sleep(SMALL_SLEEP_TIME)  # make sure that status column is created
-
-    # Check that the script ran successfully
-    assert result.returncode == 0, f"Script failed with error: {result.stderr}"
-
-    # Check that the output file was created
-    assert os.path.exists(log_file_path), "Log file was not created"
-
-    # Check that the CSV was updated
-    df = pd.read_csv(test_env["csv_path"])
-    num_sleeps = 0
-    while (
-        df["status"].iloc[0] in ["Submitted", "Running"]
-        and num_sleeps < MAX_SLEEPS
-    ):
-        # Wait a bit for the job to complete
-        time.sleep(SLEEP_TIME)
-        df = pd.read_csv(test_env["csv_path"])
-        num_sleeps += 1
-    assert df["status"].iloc[0] == "Completed", "CSV status was not updated"
-
-    # Load canonical CSV for comparison
-    canonical_df = pd.read_csv(os.path.join(CUR_FOLDER, "canonical_csv.csv"))
-
-    # Check that all expected columns exist in both dataframes
-    assert set(canonical_df.columns) == set(
-        df.columns
-    ), "CSV columns do not match canonical CSV"
-
-    # Check run_folder column exists and contains string
-    for col in UNIQUE_COLUMNS:
-        assert col in df.columns, f"{col} column missing"
-        assert isinstance(df[col].iloc[0], str), f"{col} value is not a string"
-
-    # Compare all columns except run_folder
-    cols_to_compare = [
-        col for col in canonical_df.columns if col not in UNIQUE_COLUMNS
-    ]
-    for col in cols_to_compare:
-        for row in range(TOTAL_ROWS):
-            value_in_df = df[col].iloc[row]
-            if isinstance(value_in_df, str):
-                value_in_df = value_in_df.replace(CUR_FOLDER, ".")
-            canonical_value = canonical_df[col].iloc[row]
-            assert (
-                value_in_df == canonical_value
-            ), f"Column {col} does not match canonical CSV"
-
-            # Clean up autogenerated folder
-            autogen_path = os.path.join(CUR_FOLDER, "autogenerated")
-            if os.path.exists(autogen_path):
-                shutil.rmtree(autogen_path)
+    compare_csv_to_canonical(
+        cmd,
+        log_file_path,
+        test_env["csv_path"],
+        os.path.join(CANONICAL_FOLDER, "canonical_csv.csv"),
+    )
 
 
-# @pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.skipif(SKIP_TESTS, reason="Skip tests when debugging")
 def test_sbatch_scripts(test_env):
     """Test that the sbatch scripts are created correctly."""
 
@@ -219,7 +309,7 @@ def test_sbatch_scripts(test_env):
 
     # Run the command and check for errors
     result = subprocess.run(cmd, capture_output=True, text=True)
-    canonical_path = os.path.join(CUR_FOLDER, "canonical_sbatch.txt")
+    canonical_path = os.path.join(CANONICAL_FOLDER, "canonical_sbatch.txt")
     with open(canonical_path, "r") as f:
         canonical_contents = f.read()
     for line in result.stdout.split("\n"):
@@ -271,7 +361,7 @@ def cleanup_test_env(test_env):
         shutil.rmtree(autogen_path)
 
 
-# @pytest.mark.skip(reason="Temporarily disabled")
+@pytest.mark.skipif(SKIP_TESTS, reason="Skip tests when debugging")
 def test_run_from_csv_is_callable(test_env):
     python_binary, conda_env = get_python_binary()
     cmd = f"{python_binary} -m stnd.run_from_csv"
