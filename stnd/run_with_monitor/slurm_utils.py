44b751b799c2655f0ce41d7dcd2056cda147c6b4
oshapio <643050@gmail.com>
2025-12-31 00:24:28 +0100

    add sync

--- stnd/run_with_monitor/slurm_utils.py
+++ stnd/run_with_monitor/slurm_utils.py
@@ -22,7 +22,7 @@ DEV_NULL = "/dev/null"
 DEFAULT_SLURM_ARGS_DICT = {
     # "partition": "gpu-2080ti-beegfs",
     "gpus": 1,
-    "time": "24:00:00",
+    "time": "3-00:00:00",
     "ntasks": 1,
     "cpus-per-task": 2,
     "error": DEV_NULL,
